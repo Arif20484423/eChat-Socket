6dcb5e97cc333fe6347abdff6ff18052e17413e6
Md Arif <arif.20527@sscbs.du.ac.in>
2025-07-04 09:02:48 +0530

    some changes to made to sync frontend

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { User } from "./models/models.js";
 const app = express();
-const http= require("http");
-const { Server } = require("socket.io");
-require("dotenv").config()
+import http from "http";
+import { Server } from "socket.io";
 app.use(cors());
 const server = http.createServer(app);
 
-console.log(process.env.CHATAPP_URL)
+console.log(process.env.CHATAPP_URL);
 const io = new Server(server, {
   cors: {
     origin: process.env.CHATAPP_URL,
@@ -15,36 +17,36 @@ const io = new Server(server, {
   },
 });
 
-
 io.on("connection", (socket) => {
   console.log("Connected ");
-  socket.on("join_room",(data)=>{
-    socket.join(data.room)
-    socket.user=data.room;
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+    socket.user = data.room;
     console.log("joined");
-  })
-  socket.on("message",(data)=>{
-  
-    socket.to(data.to).emit("message",{from:socket.user,message:data.message})
-  })
-  socket.on("delete",(data)=>{
-    console.log("passing delete",data.to)
-    socket.to(data.to).emit("delete",{from:socket.user,message:data.message})
-  })
-  socket.on("groupmessage",(data)=>{
-    for(let i=0;i<data.to.length;i++){
-      socket.to(data.to[i]).emit("message",{from:socket.user,message:data.message})
+  });
+  socket.on("message", (data) => {
+    console.log("REC", data.to[0]);
+    
+    socket
+      .to(data.to)
+      .emit("message", { from: socket.user, message: data.message });
+  });
+  socket.on("delete", (data) => {
+    console.log("passing delete", data.to);
+    socket
+      .to(data.to)
+      .emit("delete", { from: socket.user, message: data.message });
+  });
+  socket.on("groupmessage", (data) => {
+    console.log(data.to);
+    for (let i = 0; i < data.to.length; i++) {
+      socket
+        .to(data.to[i])
+        .emit("message", { from: socket.user, message: data.message });
     }
-    
-  })
-
-
-    
+  });
 });
 
 server.listen("4000", () => {
   console.log("Socket server running");
-  
 });
-
-
